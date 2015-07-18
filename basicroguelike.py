0d580d7cda91c48862d08256ac80c55725c1a0f0
Akhier <akherthedragonhearted@gmail.com>
2015-07-17 22:30:09 -0400

    Generalizing

--- basicroguelike.py
+++ basicroguelike.py
@@ -4,9 +4,27 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 
 
-def handle_keys():
-    global playerx, playery
+class Object:
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
 
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char,
+                                 libtcod.BKGND_NONE)
+
+    def clear(self):
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
+
+def handle_keys():
     key = libtcod.console_wait_for_keypress(True)
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
@@ -15,16 +33,16 @@ def handle_keys():
         return True
 
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        playery -= 1
+        player.move(0, -1)
 
     if libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        playery += 1
+        player.move(0, 1)
 
     if libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        playerx -= 1
+        player.move(-1, 0)
 
     if libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        playerx += 1
+        player.move(1, 0)
 
 libtcod.console_set_custom_font('terminal12x12_gs_ro.png',
                                 libtcod.FONT_TYPE_GREYSCALE |
@@ -33,15 +51,17 @@ libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT,
                           'basicroguelike', False)
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-playerx = SCREEN_WIDTH / 2
-playery = SCREEN_HEIGHT / 2
+player = Object(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', libtcod.yellow)
+objects = [npc, player]
 
 while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(con, libtcod.white)
-    libtcod.console_put_char(con, playerx, playery, '@', libtcod.BKGND_NONE)
+    for object in objects:
+        object.draw()
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
     libtcod.console_flush()
-    libtcod.console_put_char(con, playerx, playery, ' ', libtcod.BKGND_NONE)
+    for object in objects:
+        object.clear()
     exit = handle_keys()
     if exit:
         break
