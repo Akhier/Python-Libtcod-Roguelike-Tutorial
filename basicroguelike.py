a51f559bc822c1a44e2ab36fd270c9179bdab684
Akhier <akherthedragonhearted@gmail.com>
2015-11-24 21:57:15 -0500

    Dungeon building blocks

--- basicroguelike.py
+++ basicroguelike.py
@@ -4,7 +4,6 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
-color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
 
 
@@ -15,6 +14,14 @@ class Tile:
         if block_sight is None:
             block_sight = blocked
         self.block_sight = block_sight
+
+
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
 
 
 class Object:
@@ -38,6 +45,28 @@ class Object:
         libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
 
 
+def create_room(room):
+    global map
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
+
+
+def create_h_tunnel(x1, x2, y):
+    global map
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+
+def create_v_turnnel(y1, y2, x):
+    global map
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+
 def make_map():
     global map
 
@@ -45,10 +74,15 @@ def make_map():
            for y in range(MAP_HEIGHT)]
            for x in range(MAP_WIDTH)]
 
-    map[30][22].blocked = True
-    map[30][22].block_sight = True
-    map[50][22].blocked = True
-    map[50][22].block_sight = True
+    room1 = Rect(20, 15, 10, 15)
+    room2 = Rect(50, 15, 10, 15)
+    create_room(room1)
+    create_room(room2)
+
+    create_h_tunnel(25, 55, 23)
+
+    player.x = 25
+    player.y = 23
 
 
 def render_all():
