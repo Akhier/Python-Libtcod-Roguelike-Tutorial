a2425ee6a97283f34a464a1e0d6b2a7c2bdcd9f2
akhier <akhierthedragonhearted@yahoo.com>
2015-12-09 18:50:47 -0500

    Random choices
    No I won't go and redo the items here yet

--- basicroguelike.py
+++ basicroguelike.py
@@ -320,6 +320,26 @@ def make_map():
     stairs.send_to_back()
 
 
+def random_choice_index(chances):
+    dice = libtcod.random_get_int(0, 1, sum(chances))
+
+    running_sum = 0
+    choice = 0
+    for w in chances:
+        running_sum += w
+
+        if dice <= running_sum:
+            return choice
+        choice += 1
+
+
+def random_choice(chances_dict):
+    chances = chances_dict.values()
+    strings = chances_dict.keys()
+
+    return strings[random_choice_index(chances)]
+
+
 def place_objects(room):
     num_monsters = libtcod.random_get_int(0, 0, MAX_ROOM_MONSTERS)
 
