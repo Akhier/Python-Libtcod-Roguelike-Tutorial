d11fce928624729eb8a6980109d7958c1d5b779f
akhier <akherthedragonhearted@gmail.com>
2015-12-13 14:37:45 -0500

    Breaking away from the tutorial

--- basicroguelike.py
+++ basicroguelike.py
@@ -2,6 +2,8 @@ import libtcodpy as libtcod
 import math
 import textwrap
 import shelve
+from basics.Rect import Rect
+from basics.Tile import Tile
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
@@ -44,36 +46,6 @@ color_dark_wall = libtcod.Color(0, 0, 100)
 color_light_wall = libtcod.Color(130, 110, 50)
 color_dark_ground = libtcod.Color(50, 50, 150)
 color_light_ground = libtcod.Color(200, 180, 50)
-
-
-class Tile:
-
-    def __init__(self, blocked, block_sight=None):
-        self.blocked = blocked
-
-        self.explored = False
-
-        if block_sight is None:
-            block_sight = blocked
-        self.block_sight = block_sight
-
-
-class Rect:
-
-    def __init__(self, x, y, w, h):
-        self.x1 = x
-        self.y1 = y
-        self.x2 = x + w
-        self.y2 = y + h
-
-    def center(self):
-        center_x = (self.x1 + self.x2) / 2
-        center_y = (self.y1 + self.y2) / 2
-        return (center_x, center_y)
-
-    def intersect(self, other):
-        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
-                self.y1 <= other.y2 and self.y2 >= other.y1)
 
 
 class Object:
---
+++ basics/__init__.py
@@ -0,0 +1,2 @@
+import Tile
+import Rect
